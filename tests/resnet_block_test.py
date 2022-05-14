71e355ae2cbf28e4018ac1bec603c6588ad4ed7b
DWarez <ldwarezl@gmail.com>
2022-05-14 16:42:07 +0200

    changed resnet basic block test

--- tests/resnet_block_test.py
+++ tests/resnet_block_test.py
@@ -5,6 +5,8 @@ import torch
 import torch.nn
 from resnet_block import ResNetBlock
 
+_tensor = torch.ones((1, 32, 224, 224))
 block = ResNetBlock(32, 64)
 
+print(block(_tensor).shape)
 print(block)
